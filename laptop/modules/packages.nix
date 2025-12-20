7403d7c6b84927a1383f9ea8453a21cfebeaad4f
Hovirix <184756415+Hovirix@users.noreply.github.com>
2025-12-20 18:12:23 +0100

    feat(laptop): added packages

---
+++ laptop/modules/packages.nix
@@ -0,0 +1,88 @@
+{ username, pkgs, ... }:
+{
+  environment.systemPackages = with pkgs; [
+
+    # Desktop
+    sway
+    swayidle
+    swaylock
+    i3status
+    autotiling-rs
+
+    # utilities
+    mako
+    grim
+    slurp
+    fuzzel
+    impala
+    bluetui
+    wlsunset
+    libnotify
+    wf-recorder
+    wl-clipboard
+    brightnessctl
+
+    # Apps
+    imv
+    mpv
+    foot
+    tmux
+    yazi
+    btop
+    helix
+    lazygit
+    zathura
+    appflowy
+    freetube
+    ungoogled-chromium
+
+    # CLI tools
+    gh
+    zsh
+    bat
+    eza
+    fzf
+    git
+    zoxide
+    starship
+    fastfetch
+    github-copilot-cli
+  ];
+
+  users.users.${username}.packages = with pkgs; [
+
+    # Nix
+    nil
+    nixpkgs-fmt
+
+    # Python
+    uv
+    ruff
+    pyright
+
+    # Bash
+    shfmt
+    bash-language-server
+
+    # JavaScript
+    pnpm
+    nodejs
+
+    # YAML
+    yamlfix
+    yaml-language-server
+
+    # Ansible
+    ansible
+    ansible-lint
+
+    # Containers
+    dockerfmt
+    podman-tui
+    docker-language-server
+
+    # Terraform
+    terraform
+    terraform-ls
+  ];
+}
