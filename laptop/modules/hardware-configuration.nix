f7a0d030f714b4efb93002b1342b209d7602b721
Hovirix <184756415+Hovirix@users.noreply.github.com>
2026-03-02 14:35:16 +0100

    feat(laptop): enable redistributable firmware
    - Enable hardware.enableRedistributableFirmware for proprietary firmware support
    - Required for AMD CPU microcode updates and other hardware firmware

--- laptop/modules/hardware-configuration.nix
+++ laptop/modules/hardware-configuration.nix
@@ -20,4 +20,5 @@
   system.stateVersion = "24.11";
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.enableRedistributableFirmware = true;
 }
