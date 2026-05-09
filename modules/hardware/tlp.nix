e331559bda5988666e4292f0cd99f2b00fd5b1a1
Hovirix <184756415+Hovirix@users.noreply.github.com>
2026-05-09 09:27:07 +0200

    refactor(power): defer to TLP defaults

--- modules/hardware/tlp.nix
+++ modules/hardware/tlp.nix
@@ -2,44 +2,47 @@
   services.tlp = {
     enable = true;
 
-    settings = {
+    # settings = {
 
-      # Graphics
-      RADEON_POWER_PROFILE_ON_AC = "default";
-      RADEON_POWER_PROFILE_ON_BAT = "low";
+    #   # Operation and Profiles
+    #   TLP_DISABLE_DEFAULTS=0;
 
-      RADEON_DPM_STATE_ON_AC = "performance";
-      RADEON_DPM_STATE_ON_BAT = "battery";
+    #   # Graphics
+    #   RADEON_POWER_PROFILE_ON_AC = "default";
+    #   RADEON_POWER_PROFILE_ON_BAT = "low";
 
-      AMDGPU_ABM_LEVEL_ON_AC = 0;
-      AMDGPU_ABM_LEVEL_ON_BAT = 0;
+    #   RADEON_DPM_STATE_ON_AC = "performance";
+    #   RADEON_DPM_STATE_ON_BAT = "battery";
 
-      # Kernel
-      NMI_WATCHDOG = 0;
+    #   AMDGPU_ABM_LEVEL_ON_AC = 0;
+    #   AMDGPU_ABM_LEVEL_ON_BAT = 0;
 
-      # Platform
-      PLATFORM_PROFILE_ON_AC = "performance";
-      PLATFORM_PROFILE_ON_BAT = "low-power";
+    #   # Kernel
+    #   NMI_WATCHDOG = 0;
 
-      MEM_SLEEP_ON_AC = "deep";
-      MEM_SLEEP_ON_BAT = "deep";
+    #   # Platform
+    #   PLATFORM_PROFILE_ON_AC = "performance";
+    #   PLATFORM_PROFILE_ON_BAT = "low-power";
 
-      # Processor
-      CPU_DRIVER_OPMODE_ON_AC = "active";
-      CPU_DRIVER_OPMODE_ON_BAT = "active";
+    #   MEM_SLEEP_ON_AC = "deep";
+    #   MEM_SLEEP_ON_BAT = "deep";
 
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    #   # Processor
+    #   CPU_DRIVER_OPMODE_ON_AC = "active";
+    #   CPU_DRIVER_OPMODE_ON_BAT = "active";
 
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+    #   CPU_SCALING_GOVERNOR_ON_AC = "performance";
+    #   CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-      CPU_BOOST_ON_AC = 1;
-      CPU_BOOST_ON_BAT = 0;
+    #   CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+    #   CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
 
-      # Runtime Power Management and ASPM
-      PCIE_ASPM_ON_AC = "default";
-      PCIE_ASPM_ON_BAT = "powersupersave";
-    };
+    #   CPU_BOOST_ON_AC = 1;
+    #   CPU_BOOST_ON_BAT = 0;
+
+    #   # Runtime Power Management and ASPM
+    #   PCIE_ASPM_ON_AC = "default";
+    #   PCIE_ASPM_ON_BAT = "powersupersave";
+    # };
   };
 }
