c2e2e73c1eca08cb95f93689ac10e096127aba1b
Hovirix <184756415+Hovirix@users.noreply.github.com>
2026-05-21 15:45:59 +0200

    fix(wireguard): configure agenix identity

--- modules/services/wireguard.nix
+++ modules/services/wireguard.nix
@@ -1,16 +1,20 @@
-{ config, ... }:
+{ config, username, ... }:
 {
-  age.secrets.wg_config = {
-    file = ../../secrets/wg_config.age;
-    owner = "root";
-    group = "root";
-    mode = "600";
+  age = {
+    identityPaths = [ "/home/${username}/.ssh/id_ed25519" ];
+    secrets.wg_config = {
+      file = ../../secrets/wg_config.age;
+      owner = "root";
+      group = "root";
+      mode = "600";
+    };
   };
 
-  networking.wg-quick.interfaces.wg0 = {
-    autostart = false;
-    configFile = config.age.secrets.wg_config.path;
+  networking = {
+    firewall.checkReversePath = "loose";
+    wg-quick.interfaces.wg0 = {
+      autostart = false;
+      configFile = config.age.secrets.wg_config.path;
+    };
   };
-
-  services.resolved.enable = true;
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,11 @@
-{
-  "wg_config.age".publicKeys = [
+let
+  PublicKeys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBjCf1PpvoMshFkoyjFOYUJ6/pLexwEFqr29COJawkoB"
   ];
+in
+{
+  "wg_config.age" = {
+    armor = true;
+    publicKeys = PublicKeys;
+  };
 }
