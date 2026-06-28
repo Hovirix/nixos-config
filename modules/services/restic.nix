65b577d1840822cdc53ad34091262fa0f4eb93af
Hovirix <184756415+Hovirix@users.noreply.github.com>
2026-06-28 19:48:39 +0200

    refactor(services): expand restic backup scope

--- modules/services/restic.nix
+++ modules/services/restic.nix
@@ -1,48 +1,62 @@
-{ config, username, ... }:
+{ config, pkgs, username, ... }:
 let
-  repositoryHost = "truenas.home.hovirix.dev";
-  repositoryPath = "/mnt/tank/laptop_backups";
-  repositoryUser = "backup-laptop";
+  backupUser = "laptop";
+  backupHost = "truenas.home.hovirix.dev";
+  backupPath = "/mnt/tank/backups/${backupUser}";
+
+  resticPassword = config.sops.secrets."restic/password".path;
+  resticSshKey = config.sops.secrets."restic/ssh".path;
 in
 {
-  sops = {
-    secrets = {
-      "restic/password" = {
-        mode = "0400";
-        owner = username;
-      };
+  sops.secrets = {
+    "restic/password" = {
+      owner = "root";
+      mode = "0400";
+    };
 
-      "restic/ssh_key" = {
-        mode = "0400";
-        owner = username;
-      };
+    "restic/ssh" = {
+      owner = "root";
+      mode = "0400";
     };
   };
 
-  services.restic.backups.laptop = {
-    user = username;
+  environment.systemPackages = [ pkgs.restic];
+
+  services.restic.backups.main = {
+    user = "root";
     initialize = true;
-    repository = "sftp:${repositoryUser}@${repositoryHost}:${repositoryPath}";
-    passwordFile = config.sops.secrets."restic/password".path;
+
+    repository = "sftp:${backupUser}@${backupHost}:${backupPath}";
+    passwordFile = resticPassword;
 
     paths = [
+      "/etc/ssh"
+      "/home/${username}/.gnupg"
+      "/home/${username}/.password-store"
+      "/home/${username}/.ssh"
       "/home/${username}/Documents"
+      "/home/${username}/Projects"
+      "/var/lib/iwd"
+      "/var/lib/sbctl"
     ];
 
     extraOptions = [
-      "sftp.command='ssh -i ${
-        config.sops.secrets."restic/ssh_key".path
-      } -o IdentitiesOnly=yes ${repositoryUser}@${repositoryHost} -s sftp'"
+      "sftp.command='ssh -i ${resticSshKey} -o IdentitiesOnly=yes -o StrictHostKeyChecking=accept-new ${backupUser}@${backupHost} -s sftp'"
     ];
 
     extraBackupArgs = [
       "--skip-if-unchanged"
+      "--one-file-system"
     ];
 
     pruneOpts = [
       "--keep-daily=14"
       "--keep-weekly=8"
       "--keep-monthly=12"
+    ];
+
+    checkOpts = [
+      "--read-data-subset=5%"
     ];
 
     timerConfig = {
