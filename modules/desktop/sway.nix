b100e54f9b6976750409a6045a8df7a82b693568
hovirix <contact@hovirix.dev>
2026-07-25 13:01:50 +0100

    fix(desktop): migrate to systemd.user.settings.Manager

--- modules/desktop/sway.nix
+++ modules/desktop/sway.nix
@@ -22,9 +22,9 @@
     ];
   };
 
-  systemd.user.extraConfig = ''
-    DefaultEnvironment="PATH=/run/wrappers/bin:/etc/profiles/per-user/%u/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
-  '';
+  systemd.user.settings.Manager = {
+    DefaultEnvironment = [ "PATH=/run/wrappers/bin:/etc/profiles/per-user/%u/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin" ];
+  };
 
   security.pam.services.swaylock = { };
 }
